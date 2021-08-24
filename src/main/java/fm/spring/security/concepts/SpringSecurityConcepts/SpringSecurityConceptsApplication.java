7b93a94268eb1d1aa218428d0878431e29629da7
Ch Faizan Mustansar <chaudhry.mustansar@iem.fraunhofer.de>
2021-08-25 00:49:39 +0200

    Simple Spring boot application - generated from Spring initializer tool including lombok and spring starter web dependencies.

---
+++ src/main/java/fm/spring/security/concepts/SpringSecurityConcepts/SpringSecurityConceptsApplication.java
@@ -0,0 +1,14 @@
+package fm.spring.security.concepts.SpringSecurityConcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityConceptsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringSecurityConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fm/spring/security/concepts/SpringSecurityConcepts/SpringSecurityConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package fm.spring.security.concepts.SpringSecurityConcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
